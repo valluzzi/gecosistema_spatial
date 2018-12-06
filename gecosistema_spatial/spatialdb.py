2863afc6087eb44019babccd328942d52e2122da
Valerio Luzzi <valerio.luzzi@gecosistema.it>
2018-12-06 14:14:28 +0100

    version 0.0.18

--- gecosistema_spatial/spatialdb.py
+++ gecosistema_spatial/spatialdb.py
@@ -313,7 +313,7 @@ class SpatialDB(SqliteDB):
             spatialRef.ImportFromWkt(wkt)
         else:
             spatialRef.ImportFromWkt(3857)
-            
+
         return spatialRef
 
     def toShp(self, sql, env, fileshp, epsg=3857):
